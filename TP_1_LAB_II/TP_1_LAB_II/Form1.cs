dd5ee603ba24bfdb613903fa30802e39384bca06
NicoStellisano <nicostellisano@hotmail.com>
2016-09-06 10:36:40 -0300

    Actualizacion

---
+++ TP_1_LAB_II/TP_1_LAB_II/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_1_LAB_II
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TP_1_LAB_II/TP_1_LAB_II/Numero.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_1_LAB_II
+{
+    public class Numero
+    {
+        private double _numero;
+        private double _aux;
+
+        public double getNumero()
+        {
+            return this._numero;
+        }
+
+        public void Numero()
+        {
+            this._numero = 0;
+        }
+
+        public void Numero(string numero)
+        {
+            if (double.TryParse(numero, out _aux))           
+                this._numero = _aux;           
+        }
+
+        public void Numero(double numero)
+        {
+            this._numero = numero;
+        }
+
+        private void setNumero(string numero)
+        {
+            this._numero = validarNumero(numero);
+            
+        }
+
+        private double validarNumero(string numeroString)
+        {
+            if (double.TryParse(numeroString, out _aux))
+                return _aux;          
+                return 0;
+        }
+
+    }
+}
