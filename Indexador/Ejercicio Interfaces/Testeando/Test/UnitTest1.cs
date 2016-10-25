20b8d6c5d8290a8f0b48b4d14830ebce17bf41f2
unknown <alumno@LAB02PC18.labs.extension>
2016-10-25 12:03:58 -0300

    Act

---
+++ Indexador/Ejercicio Interfaces/Testeando/Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Testeando;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Debe instanciar el objeto Estacionamiento
+        /// </summary>
+        [TestMethod]
+        public void InstanciaEstacionamiento()
+        {
+            Estacionamiento est1 = new Estacionamiento();
+            Assert.IsNotNull(est1);
+        }
+
+        [TestMethod]
+        public void ListadoInstanciado()
+        {
+            Estacionamiento est2 = new Estacionamiento();
+            Assert.IsNotNull(est2.listaVehiculos);
+        }
+    }
+}
