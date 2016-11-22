a7aec6e0e51d0c53be0b5b0a045faadd9a8c5e04
unknown <alumno@LAB02PC14.labs.extension>
2016-11-22 10:22:34 -0300

    act

---
+++ Hilos/Clases_Eventos/Cuenta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Eventos
+{
+    public delegate void CuentaDelegado(float dinero);
+
+    
+    public class Cuenta
+    {
+        public Cuenta()
+        {
+
+        }
+        public float saldo;
+        public event CuentaDelegado SaldoNegativo;
+        public void SacarDinero(float dinero)
+        {
+            if (dinero > this.saldo)
+            {
+                //Sì estuvieramos en aplicacion de consola, Console.Write
+                //Sì estuvieramos en Windows Form, MessageBox.Show()
+                //Si estuvieramos en una pagina web, seria un alert()
+                this.SaldoNegativo(dinero);//Eventos son genéricos para delegar la forma que se muestra
+            }
+        }
+    }
+}
---
+++ Hilos/Eventos/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Clases_Eventos;
+
+namespace Eventos
+{
+    public partial class Form1 : Form
+    {
+        public Cuenta cuenta;
+        public Form1()
+        {
+            InitializeComponent();
+            cuenta = new Cuenta();
+            cuenta.SaldoNegativo += this.MostrarSaldoNegativo;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            this.cuenta.SacarDinero(1000);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            MessageBox.Show(e.KeyChar.ToString());
+        }
+        private void textBox1_KeyPress2(object sender, KeyPressEventArgs e)
+        {
+            MessageBox.Show("textBox1_KeyPress2");
+
+        }
+        private void textBox1_KeyPress3(object sender, KeyPressEventArgs e)
+        {
+            MessageBox.Show("textBox1_KeyPress3");
+
+        }
+
+        public void MostrarSaldoNegativo(float dinero)
+        {
+            MessageBox.Show(dinero.ToString());
+        }
+    }
+}
