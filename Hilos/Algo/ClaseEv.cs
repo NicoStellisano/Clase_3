70e0453c3a28fffc164c97a191ee358dad163b19
unknown <alumno@LAB02PC14.labs.extension>
2016-11-22 11:55:08 -0300

    act

---
+++ Hilos/Algo/ClaseEv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo
+{
+    public delegate void DelegadoClaseEv(string nombre);
+    public class ClaseEv
+    {
+       
+        public string nombre;
+        public event DelegadoClaseEv eventoNombre;
+
+        public ClaseEv(string nom)
+        {
+            this.nombre = nom;
+        }
+
+        public void MostrarNombre()
+        {
+            this.eventoNombre(this.nombre);
+        }
+    }
+}
---
+++ Hilos/Algo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+           /* ClaseEv clase = new ClaseEv("Carlitos");
+
+            clase.eventoNombre += new DelegadoClaseEv(eventoMostrarNombre);
+            clase.MostrarNombre();
+            */ 
+            string minombre = "Nicolás";
+            minombre.MiMetodo();
+            Console.ReadKey();
+        }
+
+        public static void eventoMostrarNombre(string nombre)
+        {
+            
+            Console.WriteLine(nombre);
+        }
+    }
+}
